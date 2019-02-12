20abf964610c158a7f3148bc5e212604eb1261c7
Juho Taakala <juho.taakala@cs.tamk.fi>
2019-02-12 18:54:00 +0200

    inital commit v2

---
+++ app/src/main/java/com/example/juho/dreamcrusher/LottoService.java
@@ -0,0 +1,157 @@
+package com.example.juho.dreamcrusher;
+
+import android.app.IntentService;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.annotation.Nullable;
+import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.TreeSet;
+
+public class LottoService extends IntentService {
+    private ArrayList<Integer> playersLotto;
+    private TreeSet<Integer> randomLotto;
+    private LocalBroadcastManager manager;
+    private boolean stopService;
+    private double yearsPassed;
+    private int weeksPassed;
+    private long calculationSpeed;
+    private int skillLevel;
+    private Intent broadCastIntent;
+
+    public LottoService() {
+        super("hello from lotto service");
+
+        playersLotto = new ArrayList<>();
+        randomLotto = new TreeSet<>();
+
+        manager = LocalBroadcastManager.getInstance(this);
+        broadCastIntent = new Intent("lotto");
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("lottoService"));
+
+        setStop(false);
+        setCalculationSpeed(2000);
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            calculationSpeed = intent.getLongExtra("calculationSpeed", 2000);
+            setCalculationSpeed(calculationSpeed);
+
+            stopService = intent.getBooleanExtra("stopService", false);
+            setStop(stopService);
+
+            skillLevel = intent.getIntExtra("skillLevel", 7);
+            setSkillLevel(skillLevel);
+        }
+    };
+
+    public void setYears(double years) {
+        this.yearsPassed = years;
+    }
+
+    public double getYears() {
+        return this.yearsPassed;
+    }
+
+    public void setCalculationSpeed(long speed) {
+        this.calculationSpeed = speed;
+    }
+
+    public long getCalculationSpeed() {
+        return this.calculationSpeed;
+    }
+
+    public void setStop(boolean stop) {
+        this.stopService = stop;
+    }
+
+    public boolean getStop() {
+        return this.stopService;
+    }
+
+    public void setSkillLevel(int skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public int getSkillLevel() {
+        return this.skillLevel;
+    }
+
+    @Override
+    protected void onHandleIntent(@Nullable Intent intent) {
+        weeksPassed = 0;
+        setSkillLevel(intent != null ? intent.getIntExtra("skillLevel", 7) : 0);
+        calculateLotto(intent);
+    }
+
+    public void calculateLotto(Intent intent) {
+        while (!getStop()) {
+            try {
+                Thread.sleep(getCalculationSpeed());
+
+                addComputerLotto();
+                addPlayerLotto(intent);
+
+                int correctNumbers = 0;
+
+                for (int i = 0; i < 7; i++) {
+                    if (randomLotto.contains(playersLotto.get(i))) {
+                        correctNumbers++;
+                    }
+
+                    if (correctNumbers == getSkillLevel()) {
+                        broadCastIntent.putExtra("userWon", true);
+                        setStop(true);
+                    }
+                }
+
+                for (int i = 0; i < 7; i++) {
+                    Log.d("PLAYER", "" + playersLotto.get(i));
+                }
+
+                Log.d("COMPUTER", ""  + Arrays.toString(randomLotto.toArray()));
+                Log.d("SKILL LEVEL", "" + getSkillLevel());
+                Log.d("CORRECT num", "" + correctNumbers);
+
+                weeksPassed++;
+                yearsPassed = yearsPassed + 0.0191653649;   // 1 week = 0.0191653649 years
+                setYears(yearsPassed);
+                sendBroadCast(weeksPassed);
+
+                randomLotto.clear();
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void sendBroadCast(int weeksPassed) {
+        broadCastIntent.putExtra("years", getYears());
+        broadCastIntent.putExtra("weeks", weeksPassed);
+        broadCastIntent.putExtra("numbers", randomLotto.toArray());
+        manager.sendBroadcast(broadCastIntent);
+    }
+
+    public void addComputerLotto() {
+        while (randomLotto.size() < 7) {
+            randomLotto.add((int) (Math.random() * ((39 - 1) + 1)) + 1);
+        }
+    }
+
+    public void addPlayerLotto(Intent intent) {
+        for (int i = 0; i < 7; i++) {
+            int lottoNumber = intent.getIntExtra("lottoNumbers" + i, 0);
+            playersLotto.add(lottoNumber);
+        }
+    }
+}
---
+++ app/src/main/java/com/example/juho/dreamcrusher/MainActivity.java
@@ -0,0 +1,226 @@
+package com.example.juho.dreamcrusher;
+
+import android.animation.ValueAnimator;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.graphics.PorterDuff;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.Serializable;
+import java.util.Arrays;
+import java.util.SortedSet;
+import java.util.TreeSet;
+
+public class MainActivity extends AppCompatActivity {
+    private SortedSet<Integer> lottoNumbers;
+    private TreeSet<Object> computerLotto;
+    private Intent intent;
+    private Intent toLottoService;
+    private LocalBroadcastManager manager;
+    private Button startButton;
+    private int skillLevel = 7;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intent = new Intent(this, LottoService.class);
+        toLottoService = new Intent("lottoService");
+        manager = LocalBroadcastManager.getInstance(this);
+        startButton = findViewById(R.id.startButton);
+        startButton.setEnabled(false);
+
+        lottoNumbers = new TreeSet<>();
+
+        LocalBroadcastManager.getInstance(this).registerReceiver(mMessageReceiver,
+                new IntentFilter("lotto"));
+    }
+
+    private BroadcastReceiver mMessageReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            double years = intent.getDoubleExtra("years", 0);
+            int weeks = intent.getIntExtra("weeks", 0);
+            boolean userWon = intent.getBooleanExtra("userWon", false);
+
+            if (userWon) {
+                userWonAlert();
+            }
+
+            TextView textView = findViewById(R.id.textView);
+
+            Serializable serializable = intent.getSerializableExtra("numbers");
+            Object[] object = (Object[]) serializable;
+
+            computerLotto = new TreeSet<>();
+
+            if (object != null) {
+                computerLotto.addAll(Arrays.asList(object));
+                textView.setText(String.format("%.2f years (= %d weeks) have passed", years, weeks));
+            }
+
+            animateButtons();
+        }
+    };
+
+    public void animateButtons() {
+        for (int i = 1; i < 40; i++) {
+            if (computerLotto.contains(i)) {
+                String buttonID = "button" + i;
+
+                ValueAnimator animator = ValueAnimator.ofFloat(0.2f, 1f);
+                animator.addUpdateListener(animation -> getButtonID(buttonID).setAlpha((Float) animation.getAnimatedValue()));
+
+                animator.setDuration(300);
+                animator.start();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu) {
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        switch (item.getItemId()) {
+            case R.id.plus:
+                toLottoService.putExtra("calculationSpeed", 10L);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Speed +", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.minus:
+                toLottoService.putExtra("calculationSpeed", 3000L);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Speed -", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.level4:
+                skillLevel = 4;
+                toLottoService.putExtra("skillLevel", 4);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Skill level 4", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.level5:
+                skillLevel = 5;
+                toLottoService.putExtra("skillLevel", 5);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Skill level 5", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.level6:
+                skillLevel = 6;
+                toLottoService.putExtra("skillLevel", 6);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Skill level 6", Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.level7:
+                skillLevel = 7;
+                toLottoService.putExtra("skillLevel", 7);
+                manager.sendBroadcast(toLottoService);
+
+                Toast.makeText(this, "Skill level 7", Toast.LENGTH_SHORT).show();
+                return true;
+        }
+
+        return false;
+    }
+
+    public void startGame(View view) {
+        startButton.setEnabled(false);
+        intent.putExtra("skillLevel", skillLevel);
+
+        int index = 0;
+
+        for (int number : lottoNumbers) {
+            intent.putExtra("lottoNumbers" + index, number);
+            index++;
+        }
+
+        this.startService(intent);
+    }
+
+    int numbersClicked;
+
+    public void resetGame(View view) {
+        Intent in = new Intent("lottoService");
+        in.putExtra("stopService", true);
+        manager.sendBroadcast(in);
+        numbersClicked = 0;
+        startButton.setEnabled(false);
+
+        for (int buttonNum : lottoNumbers) {
+            getButtonID("button" + buttonNum).getBackground().clearColorFilter();
+        }
+
+        lottoNumbers.clear();
+    }
+
+    public View getButtonID(String buttonID) {
+        int id = getResources().getIdentifier(buttonID, "id", getPackageName());
+
+        return findViewById(id);
+    }
+
+    public void userWonAlert() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("You won the jackpot!");
+        builder.setCancelable(true);
+
+        builder.setPositiveButton("Yes", (dialog, id) -> dialog.dismiss());
+        builder.setNegativeButton("Really?", (dialog, id) -> dialog.dismiss());
+
+        AlertDialog alert = builder.create();
+        alert.show();
+    }
+
+    public void buttonOnClick(View view) {
+        Button clickedButton = (Button) view;
+        int lottoNumber = Integer.valueOf(clickedButton.getText().toString());
+
+        if (!lottoNumbers.contains(lottoNumber) && numbersClicked <= 7) {
+            numbersClicked++;
+        }
+
+        if (numbersClicked <= 7) {
+            clickedButton.getBackground().setColorFilter(getResources().getColor(R.color.blue),
+                    PorterDuff.Mode.MULTIPLY);
+            lottoNumbers.add(lottoNumber);
+        }
+
+        if (numbersClicked == 7) {
+            startButton.setEnabled(true);
+        }
+    }
+}
